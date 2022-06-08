dbf8165e1b2c1b24008ab14ed103de40bc229c5b
LeRoy-M <lmwasela@gmail.com>
2022-06-07 17:19:08 -0700

    Fixes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main -Entry Point
+*
+* Return - Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+	for(n = 'a'; n <= 'z'; ++n)
+	{
+		_putchar('c');
+	}
+	_putchar('\n');
+	return (0);
+}
