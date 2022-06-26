6f6e15d782a5baeb5834e99002b1524b3a28c7d7
LeRoy-M <lmwasela@gmail.com>
2022-06-26 09:54:51 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Takes in two parameters
+ *
+ * @argc: First parameter passed
+ * @argv: Second parameter passed
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	float add = 0;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!(isdigit(*argv[i])))
+			{
+				printf("Error\n");
+				return (0);
+			}
+			else
+			{
+				add += atoi(argv[i]);
+			}
+		}
+		printf("%.0f\n", add);
+	}
+
+	return (0);
+}
