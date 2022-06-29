c4032599839dde51500c6499c5a479a5d2b94fa2
LeRoy-M <lmwasela@gmail.com>
2022-06-29 08:30:20 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+char strlen_NULL(char *str, int strlen);
+
 /**
  * *str_concat - Concatenates two strings
  *
@@ -18,37 +20,56 @@ char *str_concat(char *s1, char *s2)
 	i = j = 0;
 
 	if (s1 == NULL && s2 == NULL)
-	{
 		return (NULL);
-	}
-	else if (s1 != NULL || s2 != NULL)
-	{
-		do {
-			s1len++;
-		} while (s1[s1len] != '\0');
 
-		do {
-			s2len++;
-		} while (s2[s2len] != '\0');
-	}
+	s1len = strlen_NULL(s1, s1len);
+	s2len = strlen_NULL(s2, s2len);
 
 	totlen = s1len + s2len;
 	catstr = (char *) malloc((totlen + 1) * sizeof(char));
 
 	if (catstr == NULL)
-	{
 		return (NULL);
+
+	if (s1 != NULL)
+	{
+		do {
+			*(catstr + i) = s1[i];
+			i++;
+		} while (i < s1len);
+	}
+	if (s2 != NULL)
+	{
+		do {
+			*(catstr + i) = s2[j];
+			i++, j++;
+		} while (j < s2len);
 	}
 
-	do {
-		*(catstr + i) = s1[i];
-		i++;
-	} while (i < s1len);
-
-	do {
-		*(catstr + i) = s2[j];
-		i++, j++;
-	} while (j < s2len);
-
 	return (catstr);
+}
+
+/**
+ * strlen_NULL - Checks string length
+ *
+ * @str:Receives character array input
+ * @strlen: Receives integer input
+ *
+ * Return: NULL
+ */
+
+char strlen_NULL(char *str, int strlen)
+{
+	if (str != NULL)
+	{
+		do {
+			strlen++;
+		} while (str[strlen] != '\0');
+
+		return (strlen);
+	}
+	else
+	{
+		return ('\0');
+	}
 }
