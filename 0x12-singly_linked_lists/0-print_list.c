085c88b31b39f9275638198ea502ee7fdc4a1e35
LeRoy-M <lmwasela@gmail.com>
2022-07-14 10:57:10 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,27 @@
 
 size_t print_list(const list_t *h)
 {
+	int nod_cnt;
+	const list_t *count;
+
+	nod_cnt = 0;
+	count = h;
+
+	do
+	{
+		nod_cnt++;
+		/*count = count->next;*/
+	} while (count != h);
+
 	if (h->str == NULL)
 	{
 		printf("[0] (nil)\n");
 		printf("[%d] %s\n", h->next->len, h->next->str);
-		return (2);
+		return (nod_cnt);
 	}
 
 	printf("[%d] %s\n", h->len, h->str);
 	printf("[%d] %s\n", h->next->len, h->next->str);
 
-	return (2);
+	return (nod_cnt);
 }
