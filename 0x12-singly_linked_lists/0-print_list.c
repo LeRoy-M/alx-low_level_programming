8411db0e282243026347266caeef366b033e299d
LeRoy-M <lmwasela@gmail.com>
2022-07-15 02:36:19 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,23 +12,35 @@ size_t print_list(const list_t *h)
 {
 	int nod_cnt;
 	const list_t *temp;
+	unsigned int str_len;
+	char *cpy_str;
 
-	nod_cnt = 0;
+	nod_cnt = str_len = 0;
+	cpy_str = h->str;
 	temp = h;
+
+	if (h->str != NULL)
+	{
+		while (*cpy_str != '\0')
+		{
+			cpy_str++;
+			str_len++;
+		}
+	}
 
 	do {
 		temp = temp->next;
 		nod_cnt++;
 	} while (temp != NULL);
 
-	if (h->str == NULL)
+	if (cpy_str == NULL)
 	{
-		printf("[0] (nil)\n");
+		printf("[%d] (nil)\n", str_len);
 		printf("[%d] %s\n", h->next->len, h->next->str);
 		return (nod_cnt);
 	}
 
-	printf("[%d] %s\n", h->len, h->str);
+	printf("[%d] %s\n", str_len, h->str);
 	printf("[%d] %s\n", h->next->len, h->next->str);
 
 	return (nod_cnt);
