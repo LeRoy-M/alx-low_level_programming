c77350c4461edc9aac1366f13b11d4e746d3e225
LeRoy-M <lmwasela@gmail.com>
2022-07-18 10:22:57 -0700

    0.Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elments of a 'listint_t' list
+ *
+ * @h: Input from function call
+ *
+ * Return: unsigned int
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int nod_cnt;
+
+	nod_cnt = 0;
+	do {
+		printf("%i\n", h->n);
+		h = h->next;
+		nod_cnt++;
+	} while (h != NULL);
+
+	return (nod_cnt);
+}
