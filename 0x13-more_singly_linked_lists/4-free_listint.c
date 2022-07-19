e57fdf23e13dbf6084e21911d941c6b339167ad3
LeRoy-M <lmwasela@gmail.com>
2022-07-19 01:22:48 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a 'listint_t' list
+ *
+ * @head: Input from function call
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
