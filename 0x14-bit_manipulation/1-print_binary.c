e4c6011735a6375d55c98a011dedd683a7a6ffe4
LeRoy-M <lmwasela@gmail.com>
2022-07-22 04:39:52 -0700

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,39 +10,15 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, j, base2;
-	char buffer1[32], buffer2[32];
-
-	for (i = 31; i >= 0; i--)
+	if (n >> 0)
 	{
-		base2 = n >> i;
+		if (n >> 1)
+			print_binary(n >> 1);
 
-		if (base2 & 1)
-			buffer1[i] = '1';
-		else
-			buffer1[i] = '0';
+		_putchar((n & 1) + '0');
 	}
-
-	for (i = 32 - 1, j = 0; i >= 0; i--, j++)
+	else
 	{
-		buffer2[j] = buffer1[i];
-	}
-	for (i = 0; i < 32; i++)
-	{
-		if (buffer2[i] == '0')
-		{
-			for (j = 0; j < i; j++)
-			{
-				if (buffer2[j] > '0')
-					_putchar(buffer2[i]);
-
-				else if (j == 30)
-					_putchar('0');
-			}
-		}
-		else
-		{
-			_putchar(buffer2[i]);
-		}
+		_putchar('0');
 	}
 }
