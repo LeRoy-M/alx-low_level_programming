6279a75bd2db7e74eefaf241e0aac8ecb48ebfa8
LeRoy-M <lmwasela@gmail.com>
2022-08-11 11:12:27 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
+void less_98(int n);
+
 /**
- * print_to_98 - Prints all natural numbers from 'n' to '98'followed by a new line
+ * print_to_98 - Prints all natural numbers from 'n' to '98' followed
+ * by a new line
  *
  * @n: Input during function call
  *
@@ -14,42 +17,7 @@ void print_to_98(int n)
 
 	while (n < 98)
 	{
-		if (n < -9)
-		{
-			i = -n;
-			_putchar('-');
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(n + ' ');
-		}
-		else if (n < 0 && n > -10)
-		{	
-			i = -n;
-			_putchar('-');
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n == 0)
-		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n > 9)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar(n + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+		less_98(n);
 		n++;
 	}
 
@@ -75,4 +43,55 @@ void print_to_98(int n)
 	_putchar((n / 10) + '0');
 	_putchar((n % 10) + '0');
 	_putchar('\n');
+}
+
+/**
+ * less_98 - Prints all natural numbers from 'n' to '98'
+ *
+ * @n: Input during function call
+ *
+ * Return: void
+ */
+
+
+void less_98(int n)
+{
+	int i;
+
+	if (n < -9)
+	{
+		i = -n;
+		_putchar('-');
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		_putchar(',');
+		_putchar(n + ' ');
+	}
+	else if (n < 0 && n > -10)
+	{
+		i = -n;
+		_putchar('-');
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else if (n > 9)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
 }
