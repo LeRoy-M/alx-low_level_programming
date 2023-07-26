719e83087cda60e2a436a005d51b7cea001ef3ac
LeRoy-M <lmwasela@gmail.com>
2023-07-26 21:49:16 +0300

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module contains the 'island_perimeter' function"""
 
+
 def island_perimeter(grid):
     """Function that returns the perimeter of island described in grid
     Args:
